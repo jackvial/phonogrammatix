a348597a440413d70e43ff927affd7d8a9b8fd2f
Jack Vial <vialjack@gmail.com>
2015-02-15 10:27:50 -0500

    Added button click sounds

--- public/app.js
+++ public/app.js
@@ -105,7 +105,16 @@ jQuery(function($){
                     console.log('fail sound should play');
                    _this.playSound(context, masterGain, buffer);
                 });
-            });
+            });;
+
+            this.loadSound('sounds/button5.wav', context, function(buffer){
+
+                // Listen for a button click
+                App.$doc.on('click', 'button', function(){
+                    console.log('button click sound should play');
+                   _this.playSound(context, masterGain, buffer);
+                });
+            })
 
         }
     };
